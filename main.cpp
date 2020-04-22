5156795fa081d0195d60f28fb03b110b397241d1
GustavNK <gustav.knudsen96@gmail.com>
2020-04-22 14:41:16 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * Transmitter.cpp
+ *
+ * Created: 22-04-2020 14:38:49
+ * Author : Gustav
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		int x = 2;
+    }
+}
+
